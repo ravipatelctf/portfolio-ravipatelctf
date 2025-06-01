b4205d567bbd975b6d89c074d1c7ff817c991513
Ravi Shankar Patel <ravipatelCTF@gmail.com>
2025-06-01 20:43:20 +0530

    Initial commit

---
+++ theme.js
@@ -0,0 +1,34 @@
+const themeToggle = document.getElementById("theme-toggle");
+
+function setTheme(themeName) {
+  document.querySelector("html").setAttribute("data-theme", themeName);
+}
+
+function handleToggleTheme(event) {
+  if (event.target.checked) {
+    setTheme("dark");
+  } else {
+    setTheme("light");
+  }
+}
+
+function setInitialTheme() {
+  const preferDarkMode = window.matchMedia(
+    "(prefers-color-scheme: dark)"
+  ).matches;
+
+  if (preferDarkMode) {
+    themeToggle.checked = true;
+    setTheme("dark");
+  } else {
+    themeToggle.checked = false;
+    setTheme("light");
+  }
+}
+
+function showInitialTheme() {
+  setInitialTheme();
+  themeToggle.addEventListener("change", handleToggleTheme);
+}
+
+document.addEventListener("DOMContentLoaded", showInitialTheme);
